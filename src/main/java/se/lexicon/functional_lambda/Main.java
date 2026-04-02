98de8dff6f8d11a7c8388a8d4f3f6e5d20ad7957
Muthana Torsson <muthana.tursson@gmail.com>
2026-04-02 10:49:41 +0200

    Added lambda implementation

--- src/main/java/se/lexicon/functional_lambda/Main.java
+++ src/main/java/se/lexicon/functional_lambda/Main.java
@@ -1,4 +1,12 @@
 package se.lexicon.functional_lambda;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Task task = () -> System.out.println("Task executed");
+        task.execute();
+
+        task.execute();
+    }
 }
--- src/main/java/se/lexicon/functional_lambda/Task.java
+++ src/main/java/se/lexicon/functional_lambda/Task.java
@@ -1,4 +1,5 @@
 package se.lexicon.functional_lambda;
 
 public interface Task {
+    void execute();
 }
